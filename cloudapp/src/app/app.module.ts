57e65831d93b95032df5d3972dbeebb1a3bc4288
Omid Soleimani <omid.soleimani@obvsg.at>
2024-05-22 15:09:56 +0200

    add locale de-AT to the app

--- cloudapp/src/app/app.module.ts
+++ cloudapp/src/app/app.module.ts
@@ -1,4 +1,6 @@
-import { NgModule } from "@angular/core";
+import { NgModule, LOCALE_ID } from "@angular/core";
+import { registerLocaleData } from "@angular/common";
+import localeDeAt from "@angular/common/locales/de-AT";
 import { HttpClientModule } from "@angular/common/http";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
@@ -14,10 +16,17 @@ import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { MainComponent } from "./components/main/main.component";
 import { InterestedUsersComponent } from "./components/interested-users/interested-users.component";
-import { RingumlaufComponent } from './components/ringumlauf/ringumlauf.component';
+import { RingumlaufComponent } from "./components/ringumlauf/ringumlauf.component";
+
+registerLocaleData(localeDeAt);
 
 @NgModule({
-  declarations: [AppComponent, MainComponent, InterestedUsersComponent, RingumlaufComponent],
+  declarations: [
+    AppComponent,
+    MainComponent,
+    InterestedUsersComponent,
+    RingumlaufComponent,
+  ],
   imports: [
     MaterialModule,
     BrowserModule,
@@ -34,6 +43,7 @@ import { RingumlaufComponent } from './components/ringumlauf/ringumlauf.componen
       provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
       useValue: { appearance: "standard" },
     },
+    { provide: LOCALE_ID, useValue: "de-AT" },
   ],
   bootstrap: [AppComponent],
 })
